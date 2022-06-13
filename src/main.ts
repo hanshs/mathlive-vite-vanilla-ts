3a7ccb699289dc4e4665ca64e03a0c07608ca2e1
hanshs <hansh.starkopf@gmail.com>
2022-06-13 15:07:46 +0300

    update example

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import * as Mathlive from 'mathlive'
 
-// import 'mathlive/dist/mathlive.min';
 import 'mathlive/dist/mathlive-fonts.css';
 import 'mathlive/dist/mathlive-static.css';
 import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
-const options:Partial<Mathlive.MathfieldOptions> = {
+app.innerHTML = `
+  <math-field plonk-sound="none" keypress-sound="none" virtual-keyboard-mode="onfocus"></math-field>
+  <math-field plonk-sound="none" keypress-sound="none" virtual-keyboard-mode="onfocus"></math-field>
+`
+
+const options: Partial<Mathlive.MathfieldOptions> = {
   plonkSound: 'none',
   keypressSound: 'none',
   virtualKeyboardMode: 'onfocus'
