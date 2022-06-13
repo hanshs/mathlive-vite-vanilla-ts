b64e295f567964f35746068fb62c84caed9395e6
hanshs <hansh.starkopf@gmail.com>
2022-06-13 16:31:13 +0300

    reproduce the bug

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,20 @@ import './style.css'
 
 const app = document.querySelector<HTMLDivElement>('#app')!
 
-app.innerHTML = `
-  <math-field plonk-sound="none" keypress-sound="none" virtual-keyboard-mode="onfocus"></math-field>
-  <math-field plonk-sound="none" keypress-sound="none" virtual-keyboard-mode="onfocus"></math-field>
-`
-
 const options: Partial<Mathlive.MathfieldOptions> = {
   plonkSound: 'none',
   keypressSound: 'none',
-  virtualKeyboardMode: 'onfocus'
+  // virtualKeyboardMode: 'onfocus'
 }
 
 app.appendChild(new Mathlive.MathfieldElement(options))
 app.appendChild(new Mathlive.MathfieldElement(options))
+
+// comment this out and uncomment line 13 to see the difference
+document.querySelectorAll<Mathlive.MathfieldElement>('math-field').forEach(element => {
+  element.setOptions({
+    virtualKeyboardMode: 'onfocus'
+  })
+})
 
 Mathlive.makeSharedVirtualKeyboard({})
