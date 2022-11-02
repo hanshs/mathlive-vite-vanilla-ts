1c193e6809956662eaffb78a489cf216e765decc
hanshs <hansh.starkopf@gmail.com>
2022-11-02 11:25:06 +0200

    comment out unused

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,8 @@ import "mathlive/dist/mathlive-fonts.css";
 import "mathlive/dist/mathlive-static.css";
 import "./style.css";
 
-import decimalSeparatorExample from "./decimal-separator-example";
-import sharedKeyboardsExample from "./shared-keyboards-example";
+// import decimalSeparatorExample from "./decimal-separator-example";
+// import sharedKeyboardsExample from "./shared-keyboards-example";
 import multipleFocusExample from "./multiple-focus-example";
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
